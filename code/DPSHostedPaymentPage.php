0a9406458e8bd867646ba6f1678b0877357ac6a6
Ingo Schommer <ingo@silverstripe.com>
2008-12-11 05:24:32 +0000

    ENHANCEMENT Added DPSHostedPaymentPage

---
+++ code/DPSHostedPaymentPage.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @package payment_dpshosted
+ */
+class DPSHostedPaymentPage extends Page {
+	
+	static $db = array(
+		'SuccessContent' => 'HTMLText', 
+		'ErrorContent' => 'HTMLText', 
+	);
+	
+	static $defaults = array(
+		'SuccessContent' => 'Thank you',
+		'ErrorContent' => 'There has been an error.',
+	);
+	
+	function getCMSFields() {
+		$fields = parent::getCMSFields();
+		
+		$fields->removeFieldFromTab('Root.Content.Main', 'Content');
+		
+		$fields->addFieldToTab(
+			'Root.Content.Main',
+			new HtmlEditorField('SuccessContent', 'Success message')
+		);
+		$fields->addFieldToTab(
+			'Root.Content.Main',
+			new HtmlEditorField('ErrorContent', 'Error message')
+		);
+		
+		return $fields;
+	}
+	
+	function requireDefaultRecords() {
+		parent::requireDefaultRecords();
+		
+		if(!DataObject::get_one('DPSHostedPaymentPage')) {
+			$page = new DPSHostedPaymentPage();
+			$page->Title = "Payment Status";
+			$page->URLSegment = "paymentstatus";
+			$page->ShowInMenu = 0;
+			$page->ShowInSearch = 0;
+			$page->write();
+			
+			Database::alteration_message("DPSHostedPaymentPage page created","created");
+		}
+	}
+}
+
+/**
+ * @package payment_dpshosted
+ */
+class DPSHostedPaymentPage_Controller extends Page_Controller {
+	
+	function Form() {
+		$formClass = DPSHostedPayment::$payment_form_class;
+		return new $formClass(
+			$this,
+			'Form'
+		);
+	}
+	
+	function success() {
+		return $this->customise(array(
+			'Content' => $this->SuccessContent
+		))->renderWith('Page');
+	}
+	
+	function error() {
+		return $this->customise(array(
+			'Content' => $this->ErrorContent
+		))->renderWith('Page');
+	}
+	
+}
+?>
